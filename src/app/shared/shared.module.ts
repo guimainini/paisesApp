c2f0eedd483c0b049dc6a5a58a707117f9d50762
Guillermo Mainini <guimainini@gmail.com>
2021-02-08 23:15:30 -0300

    2do commit ya es de noche hasta aca llegue tengo sueño

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 
@@ -13,7 +15,10 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+    
+    
   ]
 })
 export class SharedModule { }
